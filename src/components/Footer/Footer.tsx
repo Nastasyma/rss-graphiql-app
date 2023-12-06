c9207fa8717bb9f13a285dc76a7c38db13f4fdae
maria-kashpur <kashpur.maria@icloud.com>
2023-12-07 00:08:56 +0300

    feat: add Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,24 @@
+import s from './footer.module.scss';
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className={`${s.footer} conteiner`}>
+        <div>
+          <a href="https://rs.school/" target="_blanc">
+            <img
+              src="./2993771_github_social media_icon.svg"
+              alt=""
+              height={30}
+            />
+          </a>
+        </div>
+        <div>2023</div>
+        <div>
+          <a href="https://rs.school/" target="_blanc">
+            <img src="./rs_school.svg" alt="" height={30} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
