94799eb26e53037c695a883ac6fa7a2b097fd2eb
maria-kashpur <kashpur.maria@icloud.com>
2023-12-06 23:37:44 +0300

    feat: add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ErrorBoundary from './components/error-boundary/ErrorBoundary.tsx';
 import './styles/global.scss';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.tsx';
+import './utils/firebase.ts';
+import { store } from './store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      {/* <Provider store={store}> */}
-      <RouterProvider router={router} />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ErrorBoundary>
   </React.StrictMode>
 );
