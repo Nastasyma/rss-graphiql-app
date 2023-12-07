b935abba38df7193573bad73fb773ef70d3bb1c4
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-12-07 13:34:38 +0300

    refactor: edit codemirror styles

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -12,6 +12,75 @@
 // Раздел заголовка, который можно показать или скрыть, добавленные пользователем заголовки отправляются на сервер 20 баллов
 // Раздел ответов с редактором, доступным только для чтения, в качестве средства просмотра JSON 40 баллов
 
-export default function Main() {
-  return <div>Main</div>;
+import styles from './Main.module.scss';
+import { Allotment, AllotmentHandle } from 'allotment';
+import DocIcon from '../../assets/book.svg?react';
+import PlayIcon from '../../assets/play.svg?react';
+import PrettifyIcon from '../../assets/prettify.svg?react';
+import 'allotment/dist/style.css';
+import { useRef, useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { bbedit } from '@uiw/codemirror-theme-bbedit';
+
+function MainPage() {
+  const ref = useRef<AllotmentHandle>(null!);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleDocIconClick = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <Allotment defaultSizes={[1, 3, 4, 4]} minSize={50}>
+        <Allotment.Pane minSize={60} maxSize={60}>
+          <div className={`${styles.docContainer} ${styles.container}`}>
+            <button className={styles.docButton} onClick={handleDocIconClick}>
+              <DocIcon className={styles.docIcon} />
+            </button>
+          </div>
+        </Allotment.Pane>
+        <Allotment.Pane visible={isVisible}>
+          <div className={`${styles.docDescription} ${styles.container}`}>
+            Documentation
+          </div>
+        </Allotment.Pane>
+        <Allotment.Pane minSize={300}>
+          <Allotment vertical defaultSizes={[100, 1]} ref={ref}>
+            <Allotment.Pane minSize={50}>
+              <div className={`${styles.requestContainer} ${styles.container}`}>
+                Request
+                <div className={styles.request}>
+                  <div className={styles.requestEditor}>
+                    <CodeMirror theme={bbedit} width="100%" editable={true} />
+                  </div>
+                  <div className={styles.requestButtons}>
+                    <button>
+                      <PlayIcon className={styles.icon} />
+                    </button>
+                    <button>
+                      <PrettifyIcon className={styles.icon} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </Allotment.Pane>
+            <Allotment.Pane minSize={100}>
+              <div className={`${styles.queryContainer} ${styles.container}`}>
+                Variables Headers
+                <CodeMirror theme={bbedit} width="100%" editable={true} />
+              </div>
+            </Allotment.Pane>
+          </Allotment>
+        </Allotment.Pane>
+        <Allotment.Pane minSize={50}>
+          <div className={`${styles.responseContainer} ${styles.container}`}>
+            Response
+          </div>
+        </Allotment.Pane>
+      </Allotment>
+    </div>
+  );
 }
+
+export default MainPage;
