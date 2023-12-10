231ba1177524817fce59c43ccd478e5b4ccb214e
maria-kashpur <kashpur.maria@icloud.com>
2023-12-11 00:29:01 +0300

    fix: fix config (ts alias)

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,18 @@
 /// <reference types="Vite/client" />
 
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/graphiql-app',
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
   },
   test: {
     environment: 'jsdom',
