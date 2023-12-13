332c482d5590515996244af3e4115ad649b49cb8
maria-kashpur <kashpur.maria@icloud.com>
2023-12-13 13:59:07 +0300

    fix: fix not found img

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,11 +1,12 @@
 import s from './not-found.module.scss';
 import { Link } from 'react-router-dom';
+import notFoundImg from '@assets/not-found.png'
 
 export default function NotFound() {
   return (
     <div className={`conteiner ${s.notFoundBlock}`}>
       <div>
-        <img className={s.image} src="./not-found.png" alt="not found" />
+        <img className={s.image} src={notFoundImg} alt="not found" />
         <h1 className={s.header}>Oops! That page can’t be found.</h1>
         <Link to="/" className={s.link}>
           Go to the home page →
