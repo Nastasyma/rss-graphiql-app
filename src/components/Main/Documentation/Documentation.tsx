5045cf53400d1eb5393779305e0a413bd460b778
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-12-13 19:38:33 +0300

    feat: add components to schema for documentation section

--- src/components/Main/Documentation/Documentation.tsx
+++ src/components/Main/Documentation/Documentation.tsx
@@ -1,14 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './documentation..module.scss';
 import { getSchemaTypes } from '@/utils/graphqlSchema';
-import { IField, ISchemaObject, ISchemaType } from '@/types/general';
-import { getFieldType } from '@/utils/getFieldType';
+import { ISchemaType } from '@/types/general';
 import { useAppSelector } from '@/store/store';
+import Schema from './schema/Schema/Schema';
 
 function Documentation() {
-  const [schemaObject, setSchemaObject] = useState<ISchemaObject | undefined>(
-    undefined
-  );
   const [isLoading, setIsLoading] = useState(true);
   const types = useRef<ISchemaType[]>([]);
   const tabs = useAppSelector((state) => state.tabs.tabs);
@@ -20,94 +17,17 @@ function Documentation() {
       if (tabs[activeTab].url === '') {
         return;
       }
+
       types.current = await getSchemaTypes(tabs[activeTab].url);
-
-      const rootType = types.current!.find(
-        (type) => type.name === 'Query' || type.name === 'Root'
-      );
-
-      const mutationType = types.current!.find(
-        (type) => type.name === 'Mutation'
-      );
-
-      const subscriptionType = types.current!.find(
-        (type) => type.name === 'Subscription'
-      );
-
-      setSchemaObject((prevSchemaObject) => ({
-        ...prevSchemaObject!,
-        queries: rootType,
-        mutations: mutationType,
-        subscriptions: subscriptionType,
-      }));
-      console.log('types.current', types.current);
+      // console.log('types.current', types.current);
       setIsLoading(false);
     }
     getAllTypes();
   }, [tabs, activeTab]);
-  console.log('schemaObject', schemaObject);
 
   return (
     <div className={`${styles.docDescription} ${styles.container}`}>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <span className={styles.title}>Documentation</span>
-          <p className={styles.docText}>
-            A GraphQL schema provides a root type for each kind of operation.
-          </p>
-          {schemaObject && schemaObject.queries && (
-            <div>
-              <p className={styles.docText}>QUERIES</p>
-              <ul>
-                {schemaObject.queries.fields.map((field: IField) => (
-                  <li key={field.name} className={styles.fieldBlock}>
-                    <span className={styles.field}>{field.name}</span>
-                    <span>: </span>
-                    <span className={styles.type}>
-                      {getFieldType(field.type)}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-          {schemaObject && schemaObject.mutations && (
-            <div>
-              <p className={styles.docText}>MUTATIONS</p>
-              <ul>
-                {schemaObject.mutations.fields.map((field: IField) => (
-                  <li key={field.name} className={styles.fieldBlock}>
-                    <span className={styles.field}>{field.name}</span>
-                    <span>: </span>
-                    <span className={styles.type}>
-                      {getFieldType(field.type)}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-
-          {schemaObject && schemaObject.subscriptions && (
-            <div>
-              <p className={styles.docText}>SUBSCRIPTIONS</p>
-              <ul>
-                {schemaObject.subscriptions.fields.map((field: IField) => (
-                  <li key={field.name} className={styles.fieldBlock}>
-                    <span className={styles.field}>{field.name}</span>
-                    <span>: </span>
-                    <span className={styles.type}>
-                      {getFieldType(field.type)}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </>
-      )}
+      {isLoading ? <div>Loading...</div> : <Schema types={types.current} />}
     </div>
   );
 }
---
+++ src/components/Main/Documentation/schema/Schema/Schema.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import Details from '../Details/Details';
+import Navigation from '../Navigation/Navigation';
+import { IDataItem, ISchemaType } from '@/types/general';
+import DocList from '../DocList/DocList';
+import styles from './schema.module.scss';
+
+interface ISchemaProps {
+  types: ISchemaType[];
+}
+
+function Schema({ types }: ISchemaProps) {
+  const [fields, setFields] = useState<IDataItem[]>([]);
+  console.log(fields);
+  const field = fields[fields.length - 1];
+
+  const filteredTypes = types
+    .filter(
+      (item) =>
+        item.name === 'Query' ||
+        item.name === 'Root' ||
+        item.name === 'Mutation' ||
+        item.name === 'Subscription'
+    )
+    .sort((a, b) => {
+      if (a.name === 'Query' || a.name === 'Root') return -1;
+      if (b.name === 'Query' || b.name === 'Root') return 1;
+      if (a.name === 'Mutation') return -1;
+      if (b.name === 'Mutation') return 1;
+      return 0;
+    });
+
+  const selectedField = filteredTypes
+    .map((item) => item.fields)
+    .flat()
+    .find((item) => item.name === field?.name);
+
+  const selectedType = types.find((item) => item.name === String(field?.type));
+
+  return (
+    <div>
+      {selectedType ? (
+        <>
+          <Navigation fields={fields} setData={setFields} />
+          <Details
+            field={selectedField}
+            type={selectedType}
+            setData={setFields}
+          />
+        </>
+      ) : (
+        <>
+          <span className={styles.title}>Documentation</span>
+          <p className={styles.docText}>
+            A GraphQL schema provides a root type for each kind of operation.
+          </p>
+          {filteredTypes.map((item) => (
+            <DocList
+              key={item.name}
+              title={item.name.toLocaleUpperCase()}
+              data={item.fields || []}
+              setData={setFields}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Schema;
