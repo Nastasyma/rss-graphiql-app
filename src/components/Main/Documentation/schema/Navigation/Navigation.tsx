673109d3930e728eb9448efa21436d4ccdd5fe7d
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2023-12-13 19:39:02 +0300

    feat: add navigation for documentation

---
+++ src/components/Main/Documentation/schema/Navigation/Navigation.tsx
@@ -0,0 +1,42 @@
+import { IDataItem } from '@/types/general';
+import styles from './navigation.module.scss';
+
+interface INavigationProps {
+  fields: IDataItem[];
+  setData: React.Dispatch<React.SetStateAction<IDataItem[]>>;
+}
+
+function Navigation({ fields, setData }: INavigationProps) {
+  const nav = ['Documentation', ...fields.map((field) => field.name)];
+
+  const handleClick = (name: string) => {
+    const index = nav.indexOf(name) - 1;
+    setData((prev) => prev.slice(0, index + 1));
+  };
+
+  return (
+    <div className={styles.fieldBlock}>
+      {nav.map((navItem, index) => {
+        const isLastItem = index === nav.length - 1;
+        const capitalizedNavItem =
+          navItem.charAt(0).toUpperCase() + navItem.slice(1);
+
+        return (
+          <div
+            key={navItem}
+            onClick={() => handleClick(navItem)}
+            className={
+              nav.indexOf(navItem) === nav.length - 1
+                ? styles.active
+                : styles.inactive
+            }
+          >
+            <span>{capitalizedNavItem}</span> {isLastItem ? '' : '→'}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Navigation;
