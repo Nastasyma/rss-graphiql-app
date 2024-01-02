ae3fa56688b2217830338f11c63b7f9c6aba4089
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-03 00:36:00 +0300

    feat: add warning icon

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,15 @@
 import styles from './Modal.module.scss';
+import WarningIcon from '../../assets/warning.svg?react';
 
 interface ModalProps {
   errorMessage: string;
 }
 function Modal({ errorMessage }: ModalProps): JSX.Element {
   return (
-      <div className={styles.modalWrapper}>
-        <div>{errorMessage}</div>
-      </div>
+    <div className={styles.modalWrapper}>
+      <WarningIcon />
+      <div>{errorMessage}</div>
+    </div>
   );
 }
 
